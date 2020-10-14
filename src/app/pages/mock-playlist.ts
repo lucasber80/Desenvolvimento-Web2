39a1f566b3eb2f6d5b01745871378d4fbf13479a
Lucas Bernardo <49166263+lucasber80@users.noreply.github.com>
2020-10-14 11:44:15 -0300

    aplicativo com playlist dinamica

--- src/app/pages/mock-playlist.ts
+++ src/app/pages/mock-playlist.ts
@@ -2,12 +2,12 @@ import {PlaylistInterface} from './playlist'
 import {Musica} from './musica'
 
 export const playlists:PlaylistInterface[] = [
-   {id:1,nome:'pop brasil',foto:"background-image: url(assets/teste1.png);"},
-   {id:2,nome:'brasil 360',foto:"background-image: url(assets/teste2.png);"},
-   {id:3,nome:'hip-hop',foto:"background-image: url(assets/teste3.png);"},
-   {id:4,nome:'house brasil',foto:"background-image: url(assets/teste4.png);"},
-   {id:5,nome:'nao sei',foto:"background-image: url(assets/teste5.png);"},
-   {id:6,nome:'midnight ride',foto:"background-image: url(assets/teste6.png);"},
-   {id:7,nome:'top brasil',foto:"background-image: url(assets/teste7.png);"},
-   {id:8,nome:'pagodeira',foto:"background-image: url(assets/teste8.png);"}
+   {id:1,nome:'pop brasil',foto:"assets/teste1.png"},
+   {id:2,nome:'brasil 360',foto:"assets/teste2.png"},
+   {id:3,nome:'hip-hop',foto:"assets/teste3.jpg"},
+   {id:4,nome:'house brasil',foto:"assets/teste4.png"},
+   {id:5,nome:'nao sei',foto:"assets/teste5.png"},
+   {id:6,nome:'midnight ride',foto:"assets/teste6.png"},
+   {id:7,nome:'top brasil',foto:"assets/teste7.png"},
+   {id:8,nome:'pagodeira',foto:"assets/teste8.png"}
 ];
