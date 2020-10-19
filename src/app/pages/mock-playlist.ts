c680fb0f8deae60c380582a0c10d3cbd14298cf8
Lucas Bernardo <49166263+lucasber80@users.noreply.github.com>
2020-10-19 12:54:42 -0300

    alteraçoes listar musica
    melhoras no visual do listar musica

--- src/app/pages/mock-playlist.ts
+++ src/app/pages/mock-playlist.ts
@@ -3,12 +3,12 @@ import {Musica} from './musica'
 import { Musicas } from './mock-musicas';
 
 export const playlists:PlaylistInterface[] = [
-   {id:1,nome:'pop brasil',foto:"assets/teste1.png", musicas: [Musicas[0], Musicas[2], Musicas[1]]},
-   {id:2,nome:'brasil 360',foto:"assets/teste2.png", musicas:[Musicas[4], Musicas[6]]},
-   {id:3,nome:'hip-hop',foto:"assets/teste3.jpg", musicas:[Musicas[3], Musicas[5]]},
-   {id:4,nome:'house brasil',foto:"assets/teste4.png", musicas:[]},
-   {id:5,nome:'nao sei',foto:"assets/teste5.png", musicas:[]},
-   {id:6,nome:'midnight ride',foto:"assets/teste6.png", musicas:[]},
-   {id:7,nome:'top brasil',foto:"assets/teste7.png", musicas:[]},
-   {id:8,nome:'pagodeira',foto:"assets/teste8.png", musicas:[]}
+   {id:1,nome:'pop brasil',foto:"assets/imgs/teste1.png", musicas: [Musicas[0], Musicas[2], Musicas[1]]},
+   {id:2,nome:'brasil 360',foto:"assets/imgs/teste2.png", musicas:[Musicas[4], Musicas[6]]},
+   {id:3,nome:'hip-hop',foto:"assets/imgs/teste3.jpg", musicas:[Musicas[3], Musicas[5]]},
+   {id:4,nome:'house brasil',foto:"assets/imgs/teste4.png", musicas:[]},
+   {id:5,nome:'nao sei',foto:"assets/imgs/teste5.png", musicas:[]},
+   {id:6,nome:'midnight ride',foto:"assets/imgs/teste6.png", musicas:[]},
+   {id:7,nome:'top brasil',foto:"assets/imgs/teste7.png", musicas:[]},
+   {id:8,nome:'pagodeira',foto:"assets/imgs/teste8.png", musicas:[]}
 ];
