2f93a12710773b9ef5fcbfdc80cd6c2cd2b4899d
Lucas Bernardo <49166263+lucasber80@users.noreply.github.com>
2020-11-03 11:31:04 -0300

    .

---
+++ src/app/pages/playlists/playlist.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { playlists } from './mock-playlist';
+import { PlaylistInterface } from './playlist';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaylistService {
+
+  constructor() { }
+
+  all(): PlaylistInterface[] {
+    return playlists;
+  }
+
+  insert(playlist:PlaylistInterface){
+    playlists.push(playlist);
+  }
+  get(id: number): PlaylistInterface{
+    return playlists[id];
+  }
+}
